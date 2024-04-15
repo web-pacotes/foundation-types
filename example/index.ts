f67e840396e3906d40b1119f6b83cf02ee5652fc
freitzzz <1160907@isep.ipp.pt>
2024-04-15 12:38:12 +0100

    chore: add example of number division using either monad

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,21 @@
-import sum from 'foundation-types';
+import { Either, Left, Right, fold } from 'foundation-types';
 
 function main() {
-	console.log(sum(40, 2));
+	function div_numbers(x: number, y: number) {
+		if (y === 0.0) {
+			return Left('cannot divide number by 0!');
+		}
+
+		return Right(x / y);
+	}
+
+	const computation = div_numbers(2, 0);
+
+	fold(
+		computation,
+		(l) => console.error(l),
+		(r) => console.info(`division = ${r}`)
+	);
 }
 
 main();
